32af9295e244ff83343c29dea3581b19bf81e70e
Владимир Рыбалка <vrybalka@initium.ru>
2017-09-14 13:21:32 +0300

    One threading implemented

--- SlowWorker/ViewController.swift
+++ SlowWorker/ViewController.swift
@@ -9,6 +9,8 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet var startButton: UIButton!
+    @IBOutlet var resultsTextView: UITextView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,6 +22,39 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    func fetchSomethingFromServer() -> String {
+        Thread.sleep(forTimeInterval: 1)
+        return "Hi there"
+    }
+    
+    func processData(_ data: String) -> String {
+        Thread.sleep(forTimeInterval: 2)
+        return data.uppercased()
+    }
+    
+    func calculateFirstResult(_ data: String) -> String {
+        Thread.sleep(forTimeInterval: 3)
+        return "Number of chars: \(data.characters.count)"
+    }
+    
+    func calculateSecondResult(_ data: String) -> String {
+        Thread.sleep(forTimeInterval: 4)
+        return data.replacingOccurrences(of: "E", with: "e")
+    }
+    
+    @IBAction func doWork(_ sender: AnyObject) {
+        let startTime = NSDate()
+        self.resultsTextView.text = ""
+        let fetchedData = fetchSomethingFromServer()
+        let processedData = processData(fetchedData)
+        let firstResult = calculateFirstResult(processedData)
+        let secondResult = calculateSecondResult(processedData)
+        let resultSummary = "First: [\(firstResult)]\nSecond: [\(secondResult)]"
+        
+        resultsTextView.text = resultSummary
+        let endTime = NSDate()
+        print("Comleted in \(endTime.timeIntervalSince(startTime as Date)) seconds")
+    }
 
 }
 
